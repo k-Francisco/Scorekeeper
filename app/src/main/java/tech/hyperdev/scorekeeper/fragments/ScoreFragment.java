819fa21e58b95cecf26f665730f68d2e7057a36d
k-francisco <piattosnovalays@gmail.com>
2017-07-18 22:56:30 +0800

    (4) Implement score counters

--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
@@ -14,8 +15,10 @@ import tech.hyperdev.scorekeeper.R;
  */
 public class ScoreFragment extends Fragment {
 
-    public TextView mTeamName;
+    public TextView mTeamName, mScore;
+    ImageButton mPlus, mMinus;
     String teamName;
+    int teamScore=0;
 
     public ScoreFragment() {
         // Required empty public constructor
@@ -31,6 +34,26 @@ public class ScoreFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_score, container, false);
         mTeamName = (TextView) view.findViewById(R.id.tvTeamName);
         mTeamName.setText(teamName);
+
+        mScore = (TextView) view.findViewById(R.id.textView2);
+        mPlus = (ImageButton) view.findViewById(R.id.btnPlus);
+        mMinus = (ImageButton) view.findViewById(R.id.btnMinus);
+
+        mPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                teamScore++;
+                mScore.setText(Integer.toString(teamScore));
+            }
+        });
+
+        mMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                teamScore--;
+                mScore.setText(Integer.toString(teamScore));
+            }
+        });
 
         return view;
     }
